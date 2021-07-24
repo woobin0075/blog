7ee3d4f34738ead2ed05f5f06553dcb5581145f7
woobin0075 <woobin0075@gmail.com>
2021-07-24 18:37:59 +0900

    git ingnore

---
+++ server/routes/api/auth.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import auth from '../../middleware/auth'
+import config from '../../config/index'
+const {JWT_SECRET} = config
+
+//Model
+import User from '../../models/user'
+
+const router = express.Router()
+
+//@route POST api/auth
+//@desc Auth user
+//@access Public
+
+router.post('/', (req, res) => {
+    const {email, password} = req.body;
+
+    //Simple Validation
+    if(!email || !password){
+        return res.status(400).json({msg: "모든 필드를 채워주세요"});
+    }
+    //Check for existin user
+    User.findOne({email}).then((user) => {
+        if(!user) return res.status(400).json({msg: "유저가 존재하지 않습니다."});
+
+        //Validate password
+        bcrypt.compare(password, user.password).then((isMatch) => {
+            if(!isMatch) return res.status(400).json({msg: "비밀번호가 일치하지 않습니다."})
+            jwt.sign({id:user.id}, JWT_SECRET, {expiresIn: "2 days"}, (err, token) => {
+                if(err) throw err;
+                res.json({
+                    token,
+                    user: {
+                        id: user.id,
+                        name: user.name,
+                        role: user.role
+                    }
+                })
+            })
+        })
+    })
+})
+
+router.post('/logout', (req, res) => {
+    res.json("로그아웃 성공")
+})
+
+router.get('/user', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password")
+        if(!user) throw Error("유저가 존재하지 않습니다.");
+        res.json(user);
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({msg: error.message})
+    }
+})
+export default router;
